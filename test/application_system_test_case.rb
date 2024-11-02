c071e72c0765915114597b98c2e44edef41f7aa4
Tony Drake <t27duck@gmail.com>
2024-11-02 13:38:42 -0400

    Add dev container

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,15 +3,14 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  if ENV["CAPYBARA_SERVER_PORT"]
+    served_by host: "rails-app", port: ENV["CAPYBARA_SERVER_PORT"]
 
-  def sign_in_via_browser
-    visit root_path
-
-    fill_in "password", with: TEST_PASSWORD
-
-    click_on "Sign In"
-
-    assert_text "Unread Entries"
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: {
+      browser: :remote,
+      url: "http://#{ENV.fetch('SELENIUM_HOST', nil)}:4444"
+    }
+  else
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
   end
 end
